faf5d985c76adcfff7dd35bc66045b6d6ddbb1ad
Alberto Landi Cortiñas <37026303+AlbertLandi@users.noreply.github.com>
2025-04-22 21:11:17 +0200

    Add: Files, etc.

--- test_word_model.py
+++ test_word_model.py
@@ -22,22 +22,30 @@ def get_possible_action(under_limit: int, upper_limit: int):
     return rSpeed, lSpeed
 
 def test_world_model(rob, sim):
-    world_model = load_world_model("src\models\world_model_new.joblib")
-    scaler = load_scaler("src\models\scaler_new.joblib")
-    rSpeed, lSpeed = get_possible_action(-30,30)
+    world_model = load_world_model("src/models/world_model_v3.joblib")
+    scaler = load_scaler("src/models/scaler_v3.joblib")
+    rSpeed, lSpeed = get_possible_action(-30, 30)
     initial_perception = get_perceptual_state_limited(sim)
     predicted_perception = predict_next_perceptual_state(initial_perception, (rSpeed, lSpeed), world_model, scaler)
     rob.moveWheelsByTime(rSpeed, lSpeed, 1, wait=True)
     final_perception = get_perceptual_state_limited(sim)
-    print(" Initial perception: ", initial_perception)
-    print(" Action: ", (rSpeed, lSpeed))
-    print(" Predicted perception: ", predicted_perception)
-    print(" Final perception: ", final_perception)
+
+    print("\n--- Test World Model ---")
+    print(f"Initial Perception: {initial_perception}\n")
+    print(f"Action: (rSpeed={rSpeed}, lSpeed={lSpeed})\n")
+    print(f"Predicted Perception: {predicted_perception}\n")
+    print(f"Final Perception: {final_perception}")
+
     predicted_perception_array = np.array([predicted_perception[k] for k in predicted_perception.keys()], dtype=np.float64)
     final_perception_array = np.array([final_perception[k] for k in final_perception.keys()], dtype=np.float64)
     mae = np.mean(np.abs(predicted_perception_array - final_perception_array))
-    mse = np.mean((predicted_perception_array - final_perception_array)**2)
+    mse = np.mean((predicted_perception_array - final_perception_array) ** 2)
     rmse = np.sqrt(mse)
-    print(" MAE: ", mae, ", MSE: ", mse, ", RMSE: ", rmse)
+
+    print("\n--- Metrics ---")
+    print(f"MAE: {mae:.4f}")
+    print(f"MSE: {mse:.4f}")
+    print(f"RMSE: {rmse:.4f}")
+    print("-----------------------\n")
     
     
